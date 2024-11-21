49b187b72955731e2383b15e77b7837eaee03027
LeoTerryMaster <leandrogomes.santos.terrymaster@gmail.com>
2024-11-21 09:20:54 -0300

    incremento

---
+++ incremento/index.php
@@ -0,0 +1,21 @@
+<?php
+
+$x = 25.000;
+$y = 75.000;
+
+$x++;
+echo $x;
+echo "<br>";
+$x--;
+echo $x;
+echo "<br>";
+++$x;
+echo $x;
+echo "<br>";
+--$x;
+echo $x;
+echo "<br>";
+
+echo "valor de x ". $x  . " Mil <br>";
+echo "valor de x ". $y  . " Mil <br>";
+?>
