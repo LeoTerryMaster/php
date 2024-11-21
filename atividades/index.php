06cf627f2a0ab77899bb30f1ed9345deb4b66dd6
LeoTerryMaster <leandrogomes.santos.terrymaster@gmail.com>
2024-11-21 08:56:00 -0300

    gettype

--- atividades/index.php
+++ atividades/index.php
@@ -3,6 +3,14 @@
 $x = 99;
 $y = 55;
 
+
+
+
+echo gettype($x);
+
+echo "<br>";
+
+
 echo "A soma entre 2 números é " .$x + $y. "<br>";
 echo "Multiplicação entre 2 números é " .$x * $y. "<br>";
 echo "A Divisão entre 2 números é " .$x / $y. "<br>";
@@ -23,6 +31,18 @@ $resultado =  ($x>= $y) ? "O X é maior que Y" :"O y é mairo que X";
 
 echo $resultado;
 
+echo "<br>";
+echo gettype($resultado);
+
+echo "<br>";
+
+echo gettype([]);
+
+echo "<br>";
+echo gettype(12.2);
+
+echo "<br>";
+echo gettype("Leandro");
 
 echo "<br>";
 
